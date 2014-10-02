f48e065f3fa19c9db21bdbfeba19bf4fd2be4295
Win <kylinfish@gmail.com>
2014-10-02 14:05:45 +0800

    show self item in db_admin

--- web_app/models.py
+++ web_app/models.py
@@ -12,17 +12,24 @@ class unit(models.Model):
 	state = models.BooleanField(default=False)
 	pub_date = models.DateTimeField(default=datetime.datetime.now)
 	time = models.CharField(max_length=30,null=True,blank=True)
-
+	def __str__(self):
+		return (self.title)
 class student(models.Model):
 	name = models.CharField(max_length=20)
 	card = models.CharField(max_length=30)
 	s_id = models.CharField(max_length=30)
 	c_g = models.CharField(max_length=30,null=True,blank=True)#class and grade
 	pic = models.CharField(max_length=60,null=True,blank=True)
+	def __str__(self):
+		return self.name
+
 
 class participate(models.Model):
 	ref_unit = models.ForeignKey(unit)
 	ref_std = models.ForeignKey(student)
 	sig_time = models.DateTimeField('date published', default=datetime.datetime.now)
 	state = models.CharField(max_length=20)
-	
+	def __str__(self):
+		seminar = unit.objects.filter(pk= self.ref_unit.pk)
+		name = student.objects.filter(pk= self.ref_std.pk)
+		return str(seminar) + str(name) + str(self.sig_time)
