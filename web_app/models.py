051fc398658146e579888230de3a3a99aa762086
Win <kylinfish@gmail.com>
2014-10-02 14:12:42 +0800

    str to unicode

--- web_app/models.py
+++ web_app/models.py
@@ -12,15 +12,18 @@ class unit(models.Model):
 	state = models.BooleanField(default=False)
 	pub_date = models.DateTimeField(default=datetime.datetime.now)
 	time = models.CharField(max_length=30,null=True,blank=True)
-	def __str__(self):
-		return (self.title)
+	def __unicode__(self):
+		return self.title
+
+
+
 class student(models.Model):
 	name = models.CharField(max_length=20)
 	card = models.CharField(max_length=30)
 	s_id = models.CharField(max_length=30)
 	c_g = models.CharField(max_length=30,null=True,blank=True)#class and grade
 	pic = models.CharField(max_length=60,null=True,blank=True)
-	def __str__(self):
+	def __unicode__(self):
 		return self.name
 
 
@@ -29,7 +32,7 @@ class participate(models.Model):
 	ref_std = models.ForeignKey(student)
 	sig_time = models.DateTimeField('date published', default=datetime.datetime.now)
 	state = models.CharField(max_length=20)
-	def __str__(self):
-		seminar = unit.objects.filter(pk= self.ref_unit.pk)
-		name = student.objects.filter(pk= self.ref_std.pk)
-		return str(seminar) + str(name) + str(self.sig_time)
+	def __unicode__(self):
+		seminar = unit.objects.filter(pk= self.ref_unit.pk).get()
+		std = student.objects.filter(pk= self.ref_std.pk).get()
+		return unicode(seminar.title)+" -- "+ unicode(std.name)+ " -- "+ unicode(self.sig_time)
